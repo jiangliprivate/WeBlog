20347fc4cfbc23cf39e32bb84574c1f38cd21fa0
user3 <1014780649@qq.com>
2024-04-26 09:04:19 +0800

    学习vue前端

--- weblog-vue3/vite.config.js
+++ weblog-vue3/vite.config.js
@@ -11,6 +11,8 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
+    port: 80,
+    https: false,
     proxy: {
       '/api': {
         target: 'http://localhost:8081',
